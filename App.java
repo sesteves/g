b5668e3188ee3c9284c79be0461b858aa7228038
sesteves <sroesteves@gmail.com>
2016-03-08 04:16:51 +0000

    Minor changes.

--- App.java
+++ App.java
@@ -5,8 +5,19 @@ import java.util.*;
 
 public class App {
 
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+  static Map<Integer, List<Integer>> graph = new TreeMap<Integer, List<Integer>>();
+
+  static Map<Pair<Integer, Integer>, ShortestPath> shortestPaths = new HashMap<Pair<Integer, Integer>, ShortestPath>();
+  static Map<Integer, List<ShortestPath>> rows = new HashMap<Integer, List<ShortestPath>>();
+  static Map<Integer, List<ShortestPath>> columns = new HashMap<Integer, List<ShortestPath>>();
+
+  static Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex = new HashMap<Pair<Integer, Integer>, List<ShortestPath>>();
+
+
   private static Map<Integer, List<Integer>> readGraph(BufferedReader in) {
-    Map<Integer, List<Integer>> graph = new TreeMap<Integer, List<Integer>>(); 
+
     try {
       String s;
       while (!"S".equals(s = in.readLine())) {
@@ -29,7 +40,7 @@ public class App {
     return graph;
   }
 
-  private static void populateShortestPathTable(Map<Integer, List<Integer>> graph, Map<Pair<Integer, Integer>, ShortestPath> shortestPaths, Map<Integer, List<ShortestPath>> rows, Map<Integer, List<ShortestPath>> columns, Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex) {
+  private static void populateShortestPathTable() {
     /* non-recursive
     for(Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {    
       int origNode = entry.getKey();           
@@ -57,17 +68,18 @@ public class App {
 
 
     for(Integer node : graph.keySet())
-      searchShortestPath(graph, shortestPaths, pathIndex, node, node, new HashMap<Integer, Integer>(), 0);    
+      searchShortestPath(node, node, new HashMap<Integer, Integer>(), 0);
   
   }
 
 
-  private static void addPathIndexEntries(Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex, Map<Integer, Integer> path) {
+  private static void addPathIndexEntries(Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex,
+                                          Map<Integer, Integer> path, ShortestPath shortestPath) {
 
     for(Integer orig : path.keySet()) {
       int n = orig;
       while(true) {
-        if(!newPath.containsKey(n))
+        if(!path.containsKey(n))
           break;
         int dest = path.get(n);
         Pair<Integer, Integer> p = new Pair<Integer, Integer>(orig, dest);
@@ -85,7 +97,7 @@ public class App {
   }
 
 
-  private static void searchShortestPath(Map<Integer, List<Integer>> graph, Map<Pair<Integer, Integer>, ShortestPath> shortestPaths, Map<Integer, List<ShortestPath>> rows, Map<Integer, List<ShortestPath>> columns, Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex, int origNode, int node, Map<Integer, Integer> path, int distance) {
+  private static void searchShortestPath(int origNode, int node, Map<Integer, Integer> path, int distance) {
 
     List<Integer> destNodes; 
     if(graph.containsKey(node))
@@ -102,25 +114,25 @@ public class App {
       Map<Integer, Integer> newPath = new HashMap<Integer, Integer>(path);
       if(!shortestPaths.containsKey(pair) || shortestPaths.get(pair).value > distance) {        
         newPath.put(node, destNode);
-        ShortestPath shortestPath = new ShortestPath(distance, newPath);
+        ShortestPath shortestPath = new ShortestPath(pair.left, pair.right, distance, newPath);
         shortestPaths.put(pair, shortestPath);
 
-        addPathIndexEntries(pathIndex, newPath);
+        addPathIndexEntries(pathIndex, newPath, shortestPath);
       }
 
-      searchShortestPath(graph, shortestPaths, rows, columns, pathIndex, origNode, destNode, newPath, distance); 
+      searchShortestPath(origNode, destNode, newPath, distance);
     }
   }
 
   
-  private static int processQuery(Map<Pair<Integer, Integer>, ShortestPath> shortestPaths, Pair<Integer, Integer> pair) {
+  private static int processQuery(Pair<Integer, Integer> pair) {
     if(pair.left == pair.right)
       return 0;
     ShortestPath sp = shortestPaths.get(pair);
     return sp == null ? -1 : sp.value;
   }
 
-  private static void processAdd(Map<Integer, List<Integer>> graph, Map<Pair<Integer, Integer>, ShortestPath> shortestPaths, Map<Integer, List<ShortestPath>> rows,  Map<Integer, List<ShortestPath>> columns, Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex, Pair<Integer, Integer> pair) {
+  private static void processAdd(Pair<Integer, Integer> pair) {
 
     // check if edge already exists   
     if(shortestPaths.containsKey(pair) && shortestPaths.get(pair).value == 1) {
@@ -156,9 +168,9 @@ public class App {
         // add new edge to shortest paths
         Map<Integer, Integer> path = new HashMap<Integer, Integer>();
         path.put(pair.left, pair.right);
-        ShortestPath newSp = new ShortestPath(1, path);
+        ShortestPath newSp = new ShortestPath(pair.left, pair.right, 1, path);
 
-        List<ShortestNode> piList = new ArrayList<ShortestNode>();
+        List<ShortestPath> piList = new ArrayList<ShortestPath>();
         piList.add(newSp);
 
         shortestPaths.put(new Pair<Integer, Integer>(pair.left, pair.right), newSp) ;
@@ -167,13 +179,14 @@ public class App {
         // copy dest row
         List<ShortestPath> list = rows.get(pair.right);
         for(ShortestPath sp : list) {
-          if(!sp.path.contains(pair.left)) {
+          if(!sp.path.containsKey(pair.left)) {
             Map<Integer, Integer> newPath = new HashMap<Integer, Integer>(sp.path);        
-            newPath.put(pair.left, pair.right);        
-            shortestPaths.put(new Pair<Integer, Integer>(pair.left, sp.last) , new ShortestPath(sp.value + 1, newPath));
+            newPath.put(pair.left, pair.right);
+            ShortestPath newShortestPath = new ShortestPath(pair.left, sp.last, sp.value + 1, newPath);
+            shortestPaths.put(new Pair<Integer, Integer>(pair.left, sp.last), newShortestPath);
             
             // update path index
-            addPathIndexEntries(pathIndex, newPath);
+            addPathIndexEntries(pathIndex, newPath, newShortestPath);
 
           }
         }
@@ -215,7 +228,7 @@ public class App {
 
   }
 
-  private static void readBatches(BufferedReader in, Map<Integer, List<Integer>> graph, Map<Pair<Integer, Integer>, ShortestPath> shortestPaths, Map<Integer, List<ShortestPath>> rows, Map<Integer, List<ShortestPath>> columns, Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex)  {
+  private static void readBatches()  {
 
     try {
       String s;
@@ -225,12 +238,12 @@ public class App {
         switch(opCode) {
           case 'Q':
             String[] elements = s.substring(2).split(" ");
-            int result = processQuery(shortestPaths, new Pair<Integer, Integer>(Integer.parseInt(elements[0]), Integer.parseInt(elements[1])));
+            int result = processQuery(new Pair<Integer, Integer>(Integer.parseInt(elements[0]), Integer.parseInt(elements[1])));
             System.out.println(result);
             break;
           case 'A':
             elements = s.substring(2).split(" ");
-            processAdd(graph, shortestPaths, pathIndex, new Pair<Integer, Integer>(Integer.parseInt(elements[0]), Integer.parseInt(elements[1])));
+            processAdd(new Pair<Integer, Integer>(Integer.parseInt(elements[0]), Integer.parseInt(elements[1])));
             break;
           case 'D':
         }
@@ -244,8 +257,7 @@ public class App {
 
   public static void main(String[] args) {
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));    
-    Map<Integer, List<Integer>> graph = readGraph(in);
+    readGraph(in);
 
     // debug
     for(Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
@@ -255,20 +267,14 @@ public class App {
       }
       System.out.println();
     }
-   
-    Map<Pair<Integer, Integer>, ShortestPath> shortestPaths = new HashMap<Pair<Integer, Integer>, ShortestPath>();
-    Map<Integer, List<ShortestPath>> rows = new HashMap<Integer, List<ShortestPath>>();
-    Map<Integer, List<ShortestPath>> columns = new HashMap<Integer, List<ShortestPath>>();
 
-    Map<Pair<Integer, Integer>, List<ShortestPath>> pathIndex = new HashMap<Pair<Integer, Integer>, List<ShortestPath>>();
- 
-    populateShortestPathTable(graph, shortestPaths, rows, columns, pathIndex); 
+    populateShortestPathTable();
 
     // debug
     for(Map.Entry<Pair<Integer, Integer>,ShortestPath> entry : shortestPaths.entrySet())
       System.out.println("key: " + entry.getKey() + " :: value: " + entry.getValue());
 
-    readBatches(in, graph, shortestPaths, rows, columns, pathIndex);
+    readBatches();
   }
 
 
